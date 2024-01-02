659d289f130678fdec0d650d5ba79413a9112053
Dan Taylor <dan.taylor@rising-phoenix.software>
2024-01-01 20:57:47 -0500

    Fixing the error test

--- gbb/error_test.go
+++ gbb/error_test.go
@@ -9,6 +9,11 @@ import (
 	"github.com/mortedecai/go-burn-bits/gbb"
 )
 
+type errPosition struct {
+	File string `json:"file"`
+	Line int    `json:"line"`
+}
+
 var _ = Describe("Error", func() {
 	const (
 		method1 = "hello"
@@ -16,36 +21,39 @@ var _ = Describe("Error", func() {
 
 		msg1 = "Simple error message"
 	)
-	entries := []struct {
-		context string
-		expectation string
-		method string
-		message string
-		details interface{}
-		expErrorJSON string
-	}{
-		{
-			context: "no details",
-			expectation: "should create an error with no details",
-			method: method1,
-			message: msg1,
-			expErrorJSON: fmt.Sprintf(`{"method":"%s","message":"%s"}`, method1, msg1),
-		},
-		{
-			context: "no details",
-			expectation: "should create an error with no details",
-			method: method1,
-			message: msg1,
-			details: struct{
-				File string `json:"file"`
-				Line int `json:"line"`
-			}{
-				File: "/home/foo",
-				Line: 10,
+	var (
+		fooErrPosition = errPosition{
+			File: "/home/foo",
+			Line: 10,
+		}
+		entries = []struct {
+			context      string
+			expectation  string
+			method       string
+			message      string
+			details      interface{}
+			expErrorJSON string
+			expError     string
+		}{
+			{
+				context:      "no details",
+				expectation:  "should create an error with no details",
+				method:       method1,
+				message:      msg1,
+				expErrorJSON: fmt.Sprintf(`{"method":"%s","message":"%s"}`, method1, msg1),
+				expError:     fmt.Sprintf("[%s] %s [Details: |%v|]", method1, msg1, nil),
 			},
-			expErrorJSON: fmt.Sprintf(`{"method":"%s","message":"%s","details":{"file":"/home/foo","line":10}}`, method1, msg1),
-		},
-	}
+			{
+				context:      "no details",
+				expectation:  "should create an error with no details",
+				method:       method1,
+				message:      msg1,
+				details:      fooErrPosition,
+				expErrorJSON: fmt.Sprintf(`{"method":"%s","message":"%s","details":{"file":"/home/foo","line":10}}`, method1, msg1),
+				expError:     fmt.Sprintf("[%s] %s [Details: |%v|]", method1, msg1, fooErrPosition),
+			},
+		}
+	)
 	for _, e := range entries {
 		entry := e
 		Context(entry.context, func() {
@@ -53,6 +61,7 @@ var _ = Describe("Error", func() {
 				err := gbb.CreateError(entry.method, entry.message, entry.details)
 				Expect(err).To(HaveOccurred())
 				Expect(err.JSON()).To(Equal(entry.expErrorJSON))
+				Expect(err.Error()).To(Equal(entry.expError))
 			})
 		})
 	}
