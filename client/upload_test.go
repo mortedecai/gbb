35cf92400b5e736c9b894916c9d216f6975319af
Dan Taylor <dan.taylor@rising-phoenix.software>
2024-02-12 08:36:14 -0500

    Add generic BitBurner failure response and testing for upload failures

--- client/upload_test.go
+++ client/upload_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/mortedecai/gbb/client/mocks"
+	"github.com/mortedecai/gbb/gbberror"
 	"github.com/mortedecai/gbb/models"
 	"github.com/mortedecai/gbb/response"
 	. "github.com/onsi/ginkgo/v2"
@@ -32,7 +33,7 @@ var _ = Describe("Upload", func() {
 		AfterEach(func() {
 			Client = origClient
 		})
-		It("should return not yet implemented until implementation is complete", func() {
+		It("should upload correctly", func() {
 			successResponse := response.GBBUploadFileResponse{Success: true}
 			successData, err := json.Marshal(successResponse)
 			Expect(err).ToNot(HaveOccurred())
@@ -53,6 +54,54 @@ var _ = Describe("Upload", func() {
 
 			mockClient.EXPECT().Do(gomock.AssignableToTypeOf(req)).Return(mockResp.Result(), nil)
 			Expect(HandleUpload(uo)).ToNot(HaveOccurred())
+		})
+		It("should return an error due to a non-200 response code", func() {
+			failureResponse := response.GBBFailureResponse{Success: false, Message: "doh"}
+			failureData, err := json.Marshal(failureResponse)
+			Expect(err).ToNot(HaveOccurred())
+
+			mockResp := httptest.NewRecorder()
+			mockResp.Header().Add("Content-Type", "application/json")
+			mockResp.WriteHeader(400)
+			mockResp.Write(failureData)
+
+			uo.EXPECT().Server().Return("http://localhost:9990").Times(1)
+			uo.EXPECT().ToUpload().Return([]models.GBBFileName{"testdata/upload-test-1.js"})
+			uo.EXPECT().AuthToken().Return(token).Times(1)
+			addAuth := uo.EXPECT().AddAuth(gomock.AssignableToTypeOf(req))
+			addAuth.Do(func(r *http.Request) {
+				r.Header.Add("Authorization", fmt.Sprintf("Bearer %s", uo.AuthToken()))
+				addAuth.Return(r)
+			})
+
+			mockClient.EXPECT().Do(gomock.AssignableToTypeOf(req)).Return(mockResp.Result(), nil)
+			err = HandleUpload(uo)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(gbberror.ErrRequestFailed))
+		})
+		It("should return an error due to a Success: false value even on 200", func() {
+			failureResponse := response.GBBFailureResponse{Success: false, Message: "doh"}
+			failureData, err := json.Marshal(failureResponse)
+			Expect(err).ToNot(HaveOccurred())
+
+			mockResp := httptest.NewRecorder()
+			mockResp.Header().Add("Content-Type", "application/json")
+			mockResp.WriteHeader(200)
+			mockResp.Write(failureData)
+
+			uo.EXPECT().Server().Return("http://localhost:9990").Times(1)
+			uo.EXPECT().ToUpload().Return([]models.GBBFileName{"testdata/upload-test-1.js"})
+			uo.EXPECT().AuthToken().Return(token).Times(1)
+			addAuth := uo.EXPECT().AddAuth(gomock.AssignableToTypeOf(req))
+			addAuth.Do(func(r *http.Request) {
+				r.Header.Add("Authorization", fmt.Sprintf("Bearer %s", uo.AuthToken()))
+				addAuth.Return(r)
+			})
+
+			mockClient.EXPECT().Do(gomock.AssignableToTypeOf(req)).Return(mockResp.Result(), nil)
+			err = HandleUpload(uo)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(gbberror.ErrBitBurnerFailure))
 		})
 	})
 })
